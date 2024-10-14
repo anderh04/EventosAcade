ba486351c5c6536af1c46e3cef92598c40f3b051
Alex_04 <165339451+anderh04@users.noreply.github.com>
2024-10-14 16:04:50 -0500

    Create db.php

---
+++ db.php
@@ -0,0 +1,11 @@
+<?php
+$host = 'localhost';
+$user = 'root';
+$password = '';
+$database = 'crubaru_db';
+
+$conn = new mysqli($host, $user, $password, $database);
+
+if ($conn->connect_error) {
+    die("Conexión fallida: " . $conn->connect_error);
+}
