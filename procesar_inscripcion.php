5c18a0da729ec8ea291f2e6326f8c2b10c5bef44
Alex_04 <165339451+anderh04@users.noreply.github.com>
2024-10-17 19:02:39 -0500

    Update procesar_inscripcion.php

--- procesar_inscripcion.php
+++ procesar_inscripcion.php
@@ -2,13 +2,14 @@
 include 'db.php';
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $evento_id = $_POST['evento_id'];
     $nombre = $_POST['nombre'];
+    $apellido = $_POST['apellido']; 
     $correo = $_POST['correo'];
+    $evento_id = $_POST['evento_id'];
 
-    $sql = "INSERT INTO inscripciones (evento_id, nombre, correo) VALUES (?, ?, ?)";
-    $stmt = $conn->prepare($sql);
-    $stmt->bind_param("iss", $evento_id, $nombre, $correo);
+    $sql_insert = "INSERT INTO inscripciones (evento_id, nombre, apellido, correo) VALUES (?, ?, ?, ?)";
+    $stmt = $conn->prepare($sql_insert);
+    $stmt->bind_param("isss", $evento_id, $nombre, $apellido, $correo);
 
     if ($stmt->execute()) {
         header("Location: inscribir_evento.php?id=" . $evento_id . "&success=1");
