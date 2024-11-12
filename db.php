10d62dc2d06e752318ba96abde7294bbacd97743
Alex_04 <165339451+anderh04@users.noreply.github.com>
2024-11-12 16:00:08 -0500

    Add files via upload
    Codigo -proyecto NAEI MARKET

--- db.php
+++ db.php
@@ -1,11 +1,21 @@
 <?php
-$host = 'localhost';
+
+$host = 'localhost'; 
+$db = 'comercio_electronico'; 
 $user = 'root';
-$password = '';
-$database = 'crubaru_db';
+$pass = 'informatica';
+$charset = 'utf8mb4';
 
-$conn = new mysqli($host, $user, $password, $database);
+$dsn = "mysql:host=$host;dbname=$db;charset=$charset"; 
 
-if ($conn->connect_error) {
-    die("Conexión fallida: " . $conn->connect_error);
+$options = [
+    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION, 
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC, 
+    PDO::ATTR_EMULATE_PREPARES   => false, 
+];
+
+try {
+    $pdo = new PDO($dsn, $user, $pass, $options); 
+} catch (\PDOException $e) {
+    throw new \PDOException($e->getMessage(), (int)$e->getCode());
 }
